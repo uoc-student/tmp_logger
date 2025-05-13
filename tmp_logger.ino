0fe4cf4b67a2cb77ecea476259998807eb5c4ae5
Sony Dev <sony_avil@protnmail.com>
2025-05-13 17:22:45 +0300

    BME280 disabled during deep sleep

--- tmp_logger.ino
+++ tmp_logger.ino
@@ -11,17 +11,15 @@
 #define OFFSET          4.0             // There is a 4 degrees Celsius Offset
 #define TMP_ADDR        0x76            // BME280 is connected at I2C address 0x76 (pin connected to GND)
 #define SD_CS           7               // Chip Select, can be any GPIO pin
-#define DEBUG_MODE      false           // Set to true to disable deep sleep during development (prevents serial port issues)
+#define DEBUG_MODE      true           // Set to true to disable deep sleep during development (prevents serial port issues)
 #define SQW_PIN         1               // Square Wave Generator for the RTC clock, will trigger alarm to wake up esp32
 #define DS3231_ADDR     0x68            // I²C address (to set the timer)
 #define LED_BOARD       8
 #define LOG_FILE        "/log.txt"      // Log file path on SD card
+#define BME280_VCC_PIN  10              // BME280 power pin will be managed by ESP32 GPIO
 
 Adafruit_BME280         bme;            // Create an instance of the BME280 (tmp sensor)
 RTC_DS3231              rtc;            // Create an instance of the DS3231 RTC (real time clock)
-bool                    h12 = false;    // 12 hour format
-bool                    amPM = false;   // AM / PM display
-bool                    CenturyBit;     // declare a variable to receive the Century bit for year overflow
 
 
 /*
@@ -33,7 +31,24 @@ bool                    CenturyBit;     // declare a variable to receive the Cen
 
 void setup() {
   Serial.begin(115200); // Start serial communication
+  delay(1000); // Give some time to establish serial communication
   Serial.println("******** Initializing setup() ********");
+
+  // Initialize SD card  
+  if (!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    while (1);
+  } else {
+    Serial.println("OK -> Card Mount Initialized");
+  }
+
+  uint8_t cardType = SD.cardType();
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    while (1);
+  } else {
+    Serial.println("OK -> SD Card Initialized.");
+  }
   
   pinMode(LED_BOARD, OUTPUT);
   Wire.begin(2, 3);  // SDA = GPIO 2, SCL = GPIO 3 
@@ -60,27 +75,15 @@ void setup() {
   Serial.println("OK -> RTC Alarm1 sheduled!");
 
   // Initialize the BME280 sensor
+  pinMode(BME280_VCC_PIN, OUTPUT);
+  digitalWrite(BME280_VCC_PIN, HIGH); // Power BME280
+  delay(100); // Let it stabilize
+  
   if (!bme.begin(TMP_ADDR)) { // Check if the BME280 is connected at I2C address 0x76
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
   } else {
     Serial.println("OK -> Temperature Sensor Initialized");
-  }
-
-  // Initialize SD card
-  if (!SD.begin(SD_CS)) {
-    Serial.println("Card Mount Failed");
-    while (1);
-  } else {
-    Serial.println("OK -> Card Mount Initialized");
-  }
-
-  uint8_t cardType = SD.cardType();
-  if (cardType == CARD_NONE) {
-    Serial.println("No SD card attached");
-    while (1);
-  } else {
-    Serial.println("OK -> SD Card Initialized.");
   }
 
   Serial.println("******** SETUP SUCCESSFULL ********");
@@ -121,6 +124,7 @@ void loop () {
   } else {
     Serial.println("Entering deep sleep...");
     delay(100);  // Let the message flush
+    digitalWrite(BME280_VCC_PIN, LOW); // Cut power to BME280 sensor
     esp_deep_sleep_start();
   } 
 }
